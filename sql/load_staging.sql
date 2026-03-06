5178b1b9a26511b0b30378db55a92c4797643597
Christie Boz <152922237+ChristieBoz@users.noreply.github.com>
2026-03-06 11:38:40 -0500

    Update load_staging.sql

--- sql/load_staging.sql
+++ sql/load_staging.sql
@@ -1,5 +1,5 @@
 INSERT INTO staging_schema.cleaned_transactions (
-transaction_id,
+id,
 time,
 v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18,
 v19, v20, v21, v22, v23, v24, v25, v26, v27, v28,
@@ -7,7 +7,7 @@ amount,
 class 
 )
 SELECT DISTINCT
-transaction_id,
+id,
 time,
 v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18,
 v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, amount,
