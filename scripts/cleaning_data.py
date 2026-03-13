3fac08111909c8a859313ee53926c437d8e3ef61
Christie Boz <152922237+ChristieBoz@users.noreply.github.com>
2026-03-13 12:56:17 +0000

    .

--- scripts/cleaning_data.py
+++ scripts/cleaning_data.py
@@ -3,3 +3,5 @@ from scripts.db_utils import run_sql_script
 def cleaning_data():
     print("Cleaning transactional data")
     run_sql_script("~/sql/load_staging.sql")
+
+    cleaning_data.to_csv("~/results/metrics/cleaned_transactions.csv", index=False)
